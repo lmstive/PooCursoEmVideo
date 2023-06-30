b570819e37f5101a25a62538b3118866eed0c7ec
luis <lmstive@gmail.com>
2023-06-29 22:49:00 -0300

    Continuacao

---
+++ src/aula12/Aula12.java
@@ -0,0 +1,18 @@
+package aula12;
+
+public class Aula12 {
+
+	public static void main(String[] args) {
+		Mamifero m = new Mamifero();
+		Reptil r = new Reptil();
+		Peixe p = new Peixe();
+		Ave a = new Ave();
+
+		m.setPeso(35.3f);
+		m.setCorPelo("Marrom");
+		m.alimentar();
+		m.locomover();
+		m.emitirSom();
+	}
+
+}
