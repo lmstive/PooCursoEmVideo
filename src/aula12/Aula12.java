9d70044dcdf99739fbbac4b7cf65ecc59851170b
luis <lmstive@gmail.com>
2023-07-03 09:39:04 -0300

    Aula12

--- src/aula12/Aula12.java
+++ src/aula12/Aula12.java
@@ -7,12 +7,16 @@ public class Aula12 {
 		Reptil r = new Reptil();
 		Peixe p = new Peixe();
 		Ave a = new Ave();
-
-		m.setPeso(35.3f);
-		m.setCorPelo("Marrom");
-		m.alimentar();
-		m.locomover();
-		m.emitirSom();
+		Canguru c = new Canguru();
+		Cachorro k = new Cachorro();
+		Cobra j = new Cobra();
+		Tartaruga t = new Tartaruga();
+		GoldFish g = new GoldFish();
+		Arara e = new Arara();
+		
+		c.locomover();
+		k.locomover();
+		k.emitirSom();
 	}
 
 }
